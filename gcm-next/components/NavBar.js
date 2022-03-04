eef85ac31ed20713ff65e3d02ebc5d70df9a4050
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-04 15:59:26 +0100

    add components and prepare pages

---
+++ gcm-next/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav>
+        <a>
+            GCM
+        </a>
+        <ul>
+            <li>
+                <a>All posts</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
